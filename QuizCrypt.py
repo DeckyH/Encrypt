b85879d5ebcd64b2befcc5320c287ac2776d1dc7
DeckyH <32094232+DeckyH@users.noreply.github.com>
2021-04-03 21:56:38 +0100

    removed Crypto functions and replaced with pyAesCrypt library

--- QuizCrypt.py
+++ QuizCrypt.py
@@ -1,50 +1,11 @@
+ 
 #!/usr/bin/python3
 
-import os
 import sys
+import os
 import getpass
-from Crypto import Random
-from Crypto.Cipher import AES
-from Crypto.Protocol.KDF import PBKDF2
+import pyAesCrypt
 from hashlib import sha256
-
-# AES helper functions
-def pad(s):
-    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-
-def get_private_key(password, salt):
-    salt = bytes(salt, "utf-8")
-    kdf = PBKDF2(password, salt, 64, 1000)
-    key = kdf[:32]
-    return key
-
-def encrypt(message, key, key_size=256):
-    message = pad(message)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    return iv + cipher.encrypt(message)
-
-def decrypt(ciphertext, key):
-    iv = ciphertext[:AES.block_size]
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-    return plaintext.rstrip(b"\0")
-
-def encrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
-        plaintext = fo.read()
-    enc = encrypt(plaintext, key)
-    with open(file_name + ".enc", 'wb') as fo:
-        fo.write(enc)
-
-def decrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
-        ciphertext = fo.read()
-    dec = decrypt(ciphertext, key)
-    with open(file_name[:-4], 'wb') as fo:
-        fo.write(dec)
-
-# End AES helper functions
 
 def welcome():
     print("#####################################################")
@@ -70,45 +31,44 @@ action = sys.argv[1]
 answersRaw = []
 answersPreHashed = ''
 
-# salt for use in key generation, it's fine to hard code as key will be unique
-salt = 'anyrandomtextT2YpnRLAKWc86My1234'
+isHash = getpass.getpass(prompt='Do you have a hash password? Y/N: ', stream=None)
 
-q1 = getpass.getpass(prompt='Please type the answer to Question 1 (no spaces, all lower case): ', stream=None) 
-answersRaw.append(q1)
-q2 = getpass.getpass(prompt='Please type the answer to Question 2 (no spaces, all lower case): ', stream=None) 
-answersRaw.append(q2)
-q3 = getpass.getpass(prompt='Please type the answer to Question 3 (no spaces, all lower case): ', stream=None) 
-answersRaw.append(q3)
-q4 = getpass.getpass(prompt='Please type the answer to Question 4 (no spaces, all lower case): ', stream=None) 
-answersRaw.append(q4)
-q5 = getpass.getpass(prompt='Please type the answer to Question 5 (no spaces, all lower case): ', stream=None) 
-answersRaw.append(q5)
+if isHash.upper() == 'Y':
+    password = getpass.getpass(prompt='Enter hash password: ', stream=None)
+else:
+    print("OK then, let's do the quiz...")
+    q1 = getpass.getpass(prompt='Please type the answer to Question 1 (no spaces, all lower case): ', stream=None) 
+    answersRaw.append(q1)
+    q2 = getpass.getpass(prompt='Please type the answer to Question 2 (no spaces, all lower case): ', stream=None) 
+    answersRaw.append(q2)
+    q3 = getpass.getpass(prompt='Please type the answer to Question 3 (no spaces, all lower case): ', stream=None) 
+    answersRaw.append(q3)
+    q4 = getpass.getpass(prompt='Please type the answer to Question 4 (no spaces, all lower case): ', stream=None) 
+    answersRaw.append(q4)
+    q5 = getpass.getpass(prompt='Please type the answer to Question 5 (no spaces, all lower case): ', stream=None) 
+    answersRaw.append(q5)
 
-# concat answers
-for answer in answersRaw:
-    answersPreHashed += answer
+    # concat answers
+    for answer in answersRaw:
+        answersPreHashed += answer
 
-# generate a SHA256 hash of the answers
-answersHashed = str(sha256(answersPreHashed.encode('utf-8')).hexdigest())
+    # generate a SHA256 hash of the answers (password)
+    password = str(sha256(answersPreHashed.encode('utf-8')).hexdigest())
 
-# generate key to unlock document(s)
-key = get_private_key(answersHashed, salt)
 
-# get directory containing encrypted files
-directory = input("Now enter the full directory path to where your files are stored eg. /Users/john/Desktop/enc: ")
+# encryption/decryption buffer size - 64K
+bufferSize = 64 * 1024
+
+directory = input('Now enter the full directory path to where your files are stored eg. /Users/john/Desktop/enc:  ') 
 files = os.listdir(directory)
 
-# loop through files in the directory and decrypt
 for file in files:
-    if action == 'decrypt' and 'enc' in file and 'DS_Store' not in file:
+    if action == 'decrypt' and 'aes' in file and 'DS_Store' not in file:
         print("Decrypting", directory + '/' + file)
-        decrypt_file(directory + '/' + file, key)
-    elif action == 'encrypt' and 'enc' not in file:
+        pyAesCrypt.decryptFile(directory + "/" + file, directory + "/" + file + "aes", password, bufferSize)
+    elif action == 'encrypt' and 'aes' not in file and 'enc' not in file:
         print("Encrypting", directory + '/' + file)
-        encrypt_file(directory + '/' + file, key)
-
-print("All files {0}ed".format(action))
-    
+        pyAesCrypt.encryptFile(directory + "/" + file, directory + "/" + file + "aes", password, bufferSize)
 
 
 
